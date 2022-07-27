639bd4257d92a17c219e565ea97eb0872568fcbf
Ruairi McNicholas <rorymcnicholas@gmail.com>
2022-07-27 19:37:13 +0700

    Finished Gatsby Tutorial Part 4

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+// import React
+import * as React from 'react'
+import Layout from '../components/layout'
+
+// Define components
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Ruairi">
+            <p>Learn about my baller lifestyle in Thailand!</p>
+        </Layout>
+ 
+    )
+}
+
+// Export component
+export default AboutPage
